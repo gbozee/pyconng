83c029cbac0edd61edaaaaab349965582570e9a9
Osaetin Daniel <osaetindaniel@gmail.com>
2017-03-28 22:42:34 +0100

    Fixed hover effect on :btn-link's
    Updated urls.py: urlpatterns was defined twice thereby overwriting the
    urlpatterns appended in debug mode and causing errors.

--- config/urls.py
+++ config/urls.py
@@ -9,22 +9,6 @@ from django.views.generic import TemplateView, RedirectView
 from symposion import views as symposion_views
 from django.views import defaults as default_views
 from . import views as o_views
-urlpatterns = [
-]
-if settings.DEBUG:
-    # This allows the error pages to be debugged during development, just visit
-    # these url in browser to see how these error pages look like.
-    urlpatterns += [
-        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
-        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
-        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
-        url(r'^500/$', default_views.server_error),
-    ]
-    import debug_toolbar
-
-    urlpatterns += [
-        url(r'^__debug__/', include(debug_toolbar.urls)),
-    ]
 
 urlpatterns = [
     # Examples:
@@ -53,4 +37,19 @@ urlpatterns = [
     url(r'^', include('pinax.pages.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    # This allows the error pages to be debugged during development, just visit
+    # these url in browser to see how these error pages look like.
+    urlpatterns += [
+        url(r'^400/$', default_views.bad_request, kwargs={'exception': Exception('Bad Request!')}),
+        url(r'^403/$', default_views.permission_denied, kwargs={'exception': Exception('Permission Denied')}),
+        url(r'^404/$', default_views.page_not_found, kwargs={'exception': Exception('Page not Found')}),
+        url(r'^500/$', default_views.server_error),
+    ]
+    import debug_toolbar
+
+    urlpatterns += [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ]
 
