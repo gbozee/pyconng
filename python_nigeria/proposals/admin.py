b9faa0d2f08a15a45d56224d49f6b77829feee53
biola <gbozee@gmail.com>
2017-06-18 19:41:48 +0100

    removed tutorial admin

--- python_nigeria/proposals/admin.py
+++ python_nigeria/proposals/admin.py
@@ -69,6 +69,6 @@ class TalkProposalAdmin(ProposalAdmin):
     pass
 
 
-@admin.register(models.TutorialProposal)
-class TutorialProposalAdmin(ProposalAdmin):
-    pass
+# @admin.register(models.TutorialProposal)
+# class TutorialProposalAdmin(ProposalAdmin):
+#     pass
