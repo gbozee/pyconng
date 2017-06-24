069d965ef29c7d2ec30f1533da19780ec7bcafb8
Osaetin Daniel <osaetindaniel@gmail.com>
2017-06-24 11:54:07 +0100

    Restricted export fields to "username" and "email"
    Inherited django.contrib.auth.admin.UserAdmin to retain previous
    UserAdmin functionality

--- python_nigeria/core/admin.py
+++ python_nigeria/core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 from import_export.admin import ImportExportModelAdmin
 from import_export import resources
@@ -11,9 +12,10 @@ class UserResource(resources.ModelResource):
 
     class Meta:
         model = User
+        fields = ('username', 'email')
 
 
-class UserAdmin(ImportExportModelAdmin):
+class UserAdmin(UserAdmin, ImportExportModelAdmin):
     resource_class = UserResource
 
 admin.site.register(User, UserAdmin)
