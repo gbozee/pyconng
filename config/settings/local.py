cd50a43331560488a533fda3a85d811d516e7479
biola <gbozee@gmail.com>
2017-07-21 10:23:04 +0100

    fixed all issues

--- config/settings/local.py
+++ config/settings/local.py
@@ -70,3 +70,4 @@ TEST_RUNNER = 'django.test.runner.DiscoverRunner'
 
 # Your local stuff: Below this line define 3rd party library settings
 # ------------------------------------------------------------------------------
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
