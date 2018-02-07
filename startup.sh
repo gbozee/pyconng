a77e8580ab466db245285ed156abaa9b7b81520d
biola <gbozee@gmail.com>
2018-02-07 09:40:55 +0100

    updated dependecies for project to be able to replicate

--- startup.sh
+++
@@ -1,8 +0,0 @@
-source ~/.profile
-source ~/.bash_profile
-nvm use 6
-workon tuteria
-<<<<<<< HEAD
-=======
-export DJANGO_SETTINGS_MODULE=config.local
->>>>>>> c6aaff6317f53e05a067720ebedae18fa1443d72
