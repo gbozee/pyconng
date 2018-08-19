e2b047756159de550b3f87b866b6abb5459bd3eb
Beesama <gbozee@gmail.com>
2018-08-19 12:07:55 +0100

    removed stale code and set up schedule

--- python_nigeria/static/designs/dist/js.e1125e14.js
+++ python_nigeria/static/designs/dist/js.e1125e14.js
@@ -17058,7 +17058,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52248' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64019' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
