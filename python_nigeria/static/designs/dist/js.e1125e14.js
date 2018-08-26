ab95e2aa53e1df876d0013a283f39dfc4f05ea98
Beesama <gbozee@gmail.com>
2018-08-26 06:47:38 +0100

    the ability to put the venue from the admin

--- python_nigeria/static/designs/dist/js.e1125e14.js
+++ python_nigeria/static/designs/dist/js.e1125e14.js
@@ -17058,7 +17058,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64019' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '64327' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
