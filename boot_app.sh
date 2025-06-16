6b5866d9583da43ea376814cf6360e49326b3d6d
Biola <gbozee@gmail.com>
2025-06-16 20:04:32 +0100

    push new changes

--- boot_app.sh
+++ boot_app.sh
@@ -2,6 +2,7 @@ BASE_PATH='registry.gitlab.com/tuteria/tuteria'
 IMAGE_NAME="$BASE_PATH/pyconng"
 
 WEB_CONTAINER_IMAGE_NAME="$BASE_PATH/pyconng-app"
+WEB_CONTAINER_IMAGE_NAME_BACKUP="$BASE_PATH/pyconng-app-backup"
 STATIC_CONTAINER_IMAGE_NAME="$BASE_PATH/pyconng-static"
 
 cd /home/sama/app_code/pyconng && docker build -f compose/django/Dockerfile -t=$IMAGE_NAME .
@@ -10,6 +11,9 @@ docker push $IMAGE_NAME
 
 cd /home/sama/app_code/pyconng && docker build -f compose/nginx/Dockerfile-django -t=$WEB_CONTAINER_IMAGE_NAME ./compose/nginx
 docker push $WEB_CONTAINER_IMAGE_NAME
+
+cd /home/sama/app_code/pyconng && docker build -f compose/nginx/Dockerfile-django-backup -t=$WEB_CONTAINER_IMAGE_NAME_BACKUP ./compose/nginx
+docker push $WEB_CONTAINER_IMAGE_NAME_BACKUP
 
 cd /home/sama/app_code/pyconng && docker build -f compose/design/Dockerfile -t=$STATIC_CONTAINER_IMAGE_NAME ./python_nigeria/static/designs
 docker push $STATIC_CONTAINER_IMAGE_NAME
